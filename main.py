1dd66f6c63b8e7a44551c208b3af6b80a73a7ae3
Lucianoquintela0110 <117698113+Lucianoquintela@users.noreply.github.com>
2023-09-12 10:01:20 -0300

    Update main.py

--- main.py
+++ main.py
@@ -36,32 +36,32 @@ while continuar == 'S':
                         "Sistema octal é um sistema de numeração cuja base é 8, ou seja, utiliza
                         8 símbolos para a representação de quantidade. no ocidente, estes símbo-
                         los são os algarismos arábicos. o octal foi muito utilizado em informá-
-                        tica como uma alternativa mais compacta ao binário na programação em                                               linguagem de máquina.    
+                        tica como uma alternativa mais compacta ao binário na programação em linguagem de máquina.    
                         ''')
 
                 elif pergunta == 3:
                         print(''' 
-                        "Hexadecimal é sistema numérico de base 16, denotado utilizando os                                                  símbolos 0-9 e A-F(ou a-f). por exemplo, o número decimal 79(cuja                                                  representação binária é 01001111) pode ser escrito como 4f em                                                      hexadecimal(4= 0100,f= 1111) cada dígito hexadecimal consegue                                                      representar 4 bits."
+                        "Hexadecimal é sistema numérico de base 16, denotado utilizando os símbolos 0-9 e A-F(ou a-f). por exemplo, o número decimal 79(cuja representação binária é 01001111) pode ser escrito como 4f em hexadecimal(4= 0100,f= 1111) cada dígito hexadecimal consegue representar 4 bits."
                          ''')
 
         elif opcao == 1:
                 conversor_binario = conversorDeNumerosDecimais
                 conversao = bin(conversor_binario)
-                print(f'A conversão do seu número decimal {conversorDeNumerosDecimais} para                                        binário é: {conversao [2:]}')
+                print(f'A conversão do seu número decimal {conversorDeNumerosDecimais} para binário é: {conversao [2:]}')
 
         elif opcao == 2:
                 conversor_octal = conversorDeNumerosDecimais
                 conversao = oct(conversor_octal)
-                print(f'A conversão do seu número decimal {conversorDeNumerosDecimais} para                                        octal é: {conversao [2:]}')
+                print(f'A conversão do seu número decimal {conversorDeNumerosDecimais} para octal é: {conversao [2:]}')
 
         elif opcao == 3:
                 conversor_hexadecimal = conversorDeNumerosDecimais
                 conversao = hex(conversor_hexadecimal)
-                print(f'A conversão do seu número decimal {conversorDeNumerosDecimais} para                                        hexadecimal é: {conversao.upper() [2:]}')
+                print(f'A conversão do seu número decimal {conversorDeNumerosDecimais} para hexadecimal é: {conversao.upper() [2:]}')
         else:
                 print(f'Você digitou algo errado')
 
         continuar = str(input('\nDeseja continuar? Digite a letra [s]: ')).upper()
                                  
-#                                         ConversionDec
-#                                           FuturyTec   
+# ConversionDec
+# FuturyTec     
